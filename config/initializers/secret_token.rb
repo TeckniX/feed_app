738b9c78b03bbbaa2743fd8b5669059b52272838
TeckniX <lokitek@gmail.com>
2012-05-07 17:43:52 -0400

    New feed app on 3.2

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FeedApp::Application.config.secret_token = 'c2d9fc4d6a2c87f4b7475606f6fb151941392e5da42d200a8540a232ebc9458a9f8060d0e9b070fd733f09bab4ac79ff5817495c4c8d5ceb63e7f81c814c0b4c'
