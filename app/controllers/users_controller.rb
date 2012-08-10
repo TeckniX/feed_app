c0207dccde538bc83f96ee34345fd5f40229cb31
TeckniX <lokitek@gmail.com>
2012-08-10 17:05:09 -0400

    added new users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,11 @@
 class UsersController < ApplicationController
+  before_filter :signed_in_user, only: [:index, :edit, :update]
+  before_filter :correct_user,   only: [:edit, :update]
+  
+  def index
+    @users = User.all
+  end
+  
   def new
     @user = User.new
   end
@@ -7,6 +14,8 @@ class UsersController < ApplicationController
   end
 
   def edit
+    # Moved to correct_user before filter
+    #@user = User.find(params[:id])
   end
   
   def signin
@@ -20,7 +29,7 @@ class UsersController < ApplicationController
     @user = User.new(params[:user])
     if @user.save
       sign_in @user
-      flash[:success] = "Welcome to the Sample App!"
+      flash[:success] = "Welcome to the Feed App!"
       # Handle a successful save.
       redirect_to @user
     else
@@ -28,4 +37,29 @@ class UsersController < ApplicationController
     end
   end
   
+  def update
+    # Moved to correct_user before filter
+    #@user = User.find(params[:id])
+    if @user.update_attributes(params[:user])
+      # Handle a successful update.
+      flash[:success] = "Profile updated"
+      sign_in @user
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+  
+  private
+    def signed_in_user
+      # redirect_to signin_path, notice: "Please sign in." unless signed_in?
+      unless signed_in?
+        store_location
+        redirect_to signin_path, notice: "Please sign in."
+      end
+    end
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_path) unless current_user?(@user)
+    end
 end
