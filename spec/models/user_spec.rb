a33e4bf8edd35dc791606818f828dabc0dd5ac9a
TeckniX <lokitek@gmail.com>
2012-10-30 10:36:32 -0400

    Finished user admin with edit, update and destroy

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -25,10 +25,12 @@ describe User do
   it { should respond_to(:password_digest) }
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
+  it { should respond_to(:admin) }
   it { should respond_to(:authenticate) }
   it { should respond_to(:remember_token) }
 
   it { should be_valid }
+  it { should_not be_admin }
 
   describe "when name is not present" do
     before { @user.name = " " }
@@ -114,4 +116,15 @@ describe User do
     before { @user.save }
     its(:remember_token) { should_not be_blank }
   end
+  
+  # Admin functionality
+  describe "with admin attribute set to 'true'" do
+    before do
+      @user.save!
+      @user.toggle!(:admin)
+    end
+
+    it { should be_admin }
+  end
+  
 end
